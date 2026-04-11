13415a2e1716226515c046a733b4a6ee07796406
Noct <andreev.as.job@gmail.com>
2026-04-11 12:43:27 +0900

    update codecompanion plugin

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -1,46 +1,45 @@
 return {
   "olimorris/codecompanion.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
   opts = {
+    log_level = "DEBUG",
     adapters = {
       http = {
-        vastai = function()
+        ollama = function()
           return require("codecompanion.adapters").extend("ollama", {
-            name = "vastai",
             env = {
-              url = os.getenv("VASTAI_URL") or "http://localhost:11434",
+              url = "http://1.208.108.242:11434",
+              api_key = "b003ca6cade39cac388baa1b870ae5aeece97eb7533d4215763f1bf8e5d65b9b",
             },
             headers = {
               ["Content-Type"] = "application/json",
-              ["Authorization"] = "Bearer " .. (os.getenv("VASTAI_TOKEN") or ""),
+              ["Authorization"] = "Bearer b003ca6cade39cac388baa1b870ae5aeece97eb7533d4215763f1bf8e5d65b9b",
             },
             schema = {
               model = {
-                default = os.getenv("VASTAI_MODEL") or "qwen3-coder-next:latest",
-                choices = false,
-              },
-              num_ctx = {
-                default = 32768,
+                default = "qwen3-coder:latest",
               },
             },
           })
         end,
-      },
+      }
     },
     interactions = {
       chat = {
-        adapter = "vastai",
-      },
-      inline = {
-        adapter = "vastai",
-      },
-    },
-    mcp = {
-      servers = {
-        filesystem = {
-          command = "uvx",
-          args = { "mcp-server-filesystem", vim.fn.getcwd() },
+        adapter = "ollama",
+        tools = {
+          ["files"] = {
+            enabled = true,
+          },
+          ["editor"] = {
+            enabled = true,
+          },
         },
       },
+      inline = { adapter = "ollama" },
     },
   },
 }
